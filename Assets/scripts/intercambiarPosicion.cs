fe28c751bb7af02e664e11636ce9bafc00e9341c
Juan Tareq <juantareq@MacBook-Pro-de-Juan-3.local>
2015-11-06 10:00:01 +0000

    Practica RV

---
+++ Assets/scripts/intercambiarPosicion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class intercambiarPosicion : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Jump")) {
+			Vector3 temporal = transform.position;
+			temporal = target.transform.position;
+			target.transform.position = gameObject.transform.position;
+			gameObject.transform.position = temporal;
+		}
+	}
+}
---
+++ Assets/scripts/movimiento.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((Input.GetAxis ("Horizontal") >= 0.01f) || (Input.GetAxis ("Horizontal") <= 0.01f)) {
+			gameObject.transform.Translate (new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f));
+		}
+		if ((Input.GetAxis ("Vertical") >= 0.01f) || (Input.GetAxis ("Vertical") <= 0.01f)) {
+			gameObject.transform.Translate (new Vector3 (0.0f, 0.0f, Input.GetAxis ("Vertical")));
+		}
+		if (Input.GetButton("Jump")) Impulso();
+		
+	}
+	
+	void Impulso()
+	{
+		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3 (0.0f, 0.2f, 0.0f), ForceMode.Impulse);
+	}
+
+}
